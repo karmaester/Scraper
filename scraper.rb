7459a3b5523fe07afe6d045fa7d1fc8ff40bcb27
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-10-16 16:23:24 -0300

    open doc with URI

--- scraper.rb
+++ scraper.rb
@@ -1,2 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
+
+doc = Nokogiri::HTML(URI.open('https://listado.mercadolibre.cl/audifonos-sennheiser'))
+
+puts doc.xpath("//span")
+
+doc.css('price-tag-fraction').each do |link|
+  puts link.content
+end
