48a9024ad89a816e29b9be433b7794b1edb3b64e
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-10-19 17:28:12 -0300

    Turn prices into integers

--- scraper.rb
+++ scraper.rb
@@ -7,10 +7,10 @@ def scraper(url)
     prices = []
     elements = doc.css('span.ui-search-search-result__quantity-results').text.to_i
     per_page = doc.css('li.ui-search-layout__item').count
+    total_pages = (elements / per_page).round
     doc.css('li.ui-search-layout__item div.ui-search-price__second-line span.price-tag span.price-tag-fraction').each_with_index do |price, index|
-      if index.even?
-        puts price.text
-        prices.push(price)
+      if index.even? && !price.nil? # since the not evens are the payment cuotes
+        prices.push(price.text.delete('.').to_i)
       end
     end
     byebug
@@ -19,6 +19,3 @@ end
 # scraper('https://listado.mercadolibre.cl/doctor-who')
 
 scraper('https://listado.mercadolibre.cl/audifonos-sennheiser')
-
-https://listado.mercadolibre.cl/audio-audifonos/sennheiser/audifonos-sennheiser_Desde_51
-https://listado.mercadolibre.cl/audio-audifonos/sennheiser/audifonos-sennheiser_Desde_151
