48c80954c93b6fd5c0daff7edf34f0d9982580ad
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-10-19 18:03:39 -0300

    Return total_pages and prices

--- scraper.rb
+++ scraper.rb
@@ -3,19 +3,16 @@ require 'open-uri'
 require 'byebug'
 
 def scraper(url)
-    doc = Nokogiri::HTML(URI.open(url))
-    prices = []
-    elements = doc.css('span.ui-search-search-result__quantity-results').text.to_i
-    per_page = doc.css('li.ui-search-layout__item').count
-    total_pages = (elements / per_page).round
-    doc.css('li.ui-search-layout__item div.ui-search-price__second-line span.price-tag span.price-tag-fraction').each_with_index do |price, index|
-      if index.even? && !price.nil? # since the not evens are the payment cuotes
-        prices.push(price.text.delete('.').to_i)
-      end
-    end
-    byebug
+  doc = Nokogiri::HTML(URI.open(url))
+  prices = []
+  elements = doc.css('span.ui-search-search-result__quantity-results').text.to_i
+  per_page = doc.css('li.ui-search-layout__item').count
+  total_pages = (elements / per_page).round
+  doc.css('li.ui-search-layout__item div.ui-search-price__second-line span.price-tag span.price-tag-fraction')
+    .each_with_index do |price, index|
+    prices.push(price.text.delete('.').to_i) if index.even? && !price.nil? # since the not evens are the payment cuotes
+  end
+  [total_pages, prices]
 end
 
-# scraper('https://listado.mercadolibre.cl/doctor-who')
-
-scraper('https://listado.mercadolibre.cl/audifonos-sennheiser')
+p scraper('https://listado.mercadolibre.cl/doctor-who')
