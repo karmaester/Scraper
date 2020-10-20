9e37532b97d5889a5571337e355c7a418da4f0d4
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-10-20 19:13:43 -0300

    Test scraper method with url

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -2,9 +2,10 @@ require_relative '../lib/scraper_methods.rb'
 
 describe Scraper do
     let(:program) { Scraper.new }
+    let(:url) { 'https://listado.mercadolibre.cl/doctor-who' }
     describe '#scraper' do
         it 'returns an array' do
-            expect(program.scraper).to be_an_instance_of(Array)
+            expect(program.scraper(url)).to be_an_instance_of(Array)
         end
     end
 end
