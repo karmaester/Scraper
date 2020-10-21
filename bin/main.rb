13c43b0286b60419f811724702faf520b6e28e5f
Karmaester <karmaester@LAPTOP-P84UAG4M.localdomain>
2020-10-21 16:18:23 -0300

    Change screenshots and programm outputs

--- bin/main.rb
+++ bin/main.rb
@@ -3,7 +3,12 @@ require_relative '../lib/scraper_methods'
 program = Scraper.new
 puts 'Input a search for an item in mercadolibre you want the prices for'
 input = gets.chomp.strip.downcase
+puts 'Hold tight, this could take a while...'
 prices = program.get_prices("https://listado.mercadolibre.cl/#{input}")
 
-puts 'Prices for this item are:'
-print prices
+print 'Highest price for this item is:'
+puts prices.max
+print 'Lowest price for this item is: '
+puts prices.min
+print 'Number of prices checked '
+puts prices.count
